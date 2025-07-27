3a3ed74ef97a575c65d0f9ee49d8a9cb9a7026b2
AgungDH <agungdh@live.com>
2025-07-27 18:47:54 +0700

    refactor(guest): convert layout to use sections for title, breadcrumb & content
    - Replace static header `<h1>` and hard-coded breadcrumbs in `layouts/guest.blade.php` with `@yield('title')` and `@yield('breadcrumb')`
    - Remove sample cards and control sidebar markup; use `@yield('content')` for page body
    - Add `@stack('scripts')` before closing `</body>` for page-specific scripts
    - Update `pages/input-surat-masuk.blade.php` to define `title`, `breadcrumb`, and `content` sections accordingly

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -57,13 +57,11 @@
             <div class="container">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0"> Top Navigation <small>Example 3.0</small></h1>
+                        <h1>@yield('title')</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item"><a href="#">Layout</a></li>
-                            <li class="breadcrumb-item active">Top Navigation</li>
+                            @yield('breadcrumb')
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -74,75 +72,12 @@
         <!-- Main content -->
         <div class="content">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-
-                                <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's
-                                    content.
-                                </p>
-
-                                <a href="#" class="card-link">Card link</a>
-                                <a href="#" class="card-link">Another link</a>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-
-                                <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's
-                                    content.
-                                </p>
-                                <a href="#" class="card-link">Card link</a>
-                                <a href="#" class="card-link">Another link</a>
-                            </div>
-                        </div><!-- /.card -->
-                    </div>
-                    <!-- /.col-md-6 -->
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-header">
-                                <h5 class="card-title m-0">Featured</h5>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">Special title treatment</h6>
-
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h5 class="card-title m-0">Featured</h5>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">Special title treatment</h6>
-
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.col-md-6 -->
-                </div>
-                <!-- /.row -->
+                @yield('content')
             </div><!-- /.container-fluid -->
         </div>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
 
     <footer class="main-footer">
         <strong>Copyright &copy; {{ date('Y') }} {{config('app.name')}}.</strong> All rights reserved.
@@ -183,5 +118,6 @@
 <script src="/assets/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/assets/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+@stack('scripts')
 </body>
 </html>
